e86e19eaf20320cce15ee19207d8364b42f1f8f3
haseomg <lovethispool@kakao.com>
2022-12-23 00:21:34 +0900

    Initial commit

---
+++ app/src/main/java/com/example/server_conn_exam/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.server_conn_exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
