61b3881f65df6cc550f4a3f2885af9b5727a7dc9
haseomg <lovethispool@kakao.com>
2023-01-06 09:57:44 +0900

    empty commit

--- app/src/main/java/com/example/server_conn_exam/MainActivity.java
+++ app/src/main/java/com/example/server_conn_exam/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     // 여기에서 스트리밍 서버와 통신 해볼거야!
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
